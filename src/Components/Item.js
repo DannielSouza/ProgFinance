26c61184ac21c4d8b224a511eaf715bf1ad9a50d
DannielSouza <104663666+DannielSouza@users.noreply.github.com>
2022-08-23 18:42:09 -0300

    delete item bug correction

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -18,7 +18,7 @@ const Item = ({task , tasks, setTasks}) => {
 
       tasks.splice(thisItem, 1)
       if(tasks.length === 0){
-        window.localStorage.deleteItem('tasks')
+        window.localStorage.setItem('tasks', null)
         document.location.reload()
       }
       setTasks(tasks)
